08f589835891b4a0adaa4e12d359b360d16fc748
vinayvyas05 <vyasvinay95@gmail.com>
2025-11-19 19:56:50 +0530

    Add db connection and server.js

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import { connectDB } from "./config/database.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
